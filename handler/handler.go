2ce8f8a6e69233a17f4805b71f70868efd744f1d
VituSuperMEg <vitorpires8555@gmail.com>
2024-01-06 16:18:30 -0300

    new struct

--- handler/handler.go
+++ handler/handler.go
@@ -1,33 +1,5 @@
 package handler
 
-import (
-	"net/http"
+func handler() {
 
-	"github.com/gin-gonic/gin"
-)
-
-func ListOpeningHanlder(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "Todas",
-	})
-}
-func CreateOpeningHanlder(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "Post",
-	})
-}
-func ShowOpeningHanlder(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "Get",
-	})
-}
-func DeleteOpeningHanlder(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "Delete",
-	})
-}
-func UpdateOpeningHanlder(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "Update",
-	})
 }
