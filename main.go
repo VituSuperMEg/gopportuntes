dcbf19fd3e5a76cc55cc59aca0765e68441689ff
VituSuperMEg <vitorpires8555@gmail.com>
2024-01-06 16:58:11 -0300

    opening schemas & add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/VituSuperMEg/gopportuntes/router"
+import (
+	"github.com/VituSuperMEg/gopportuntes/config"
+	"github.com/VituSuperMEg/gopportuntes/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Init Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config init error: %v", err)
+		return
+	}
+
 	// Initialize Router
 	router.Initialize()
 }
